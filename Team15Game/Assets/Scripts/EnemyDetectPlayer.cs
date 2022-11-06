3e8185f5742e68aa00155c92e27e65c5f10daea2
Jacek112 <jacekteodorczyk01@gmail.com>
2022-11-07 00:23:16 +0100

    Zaimplementowanie lepszego AI - omijanie przeszkód i podążanie za graczem zrealizowane za pomocą A* Pathfinding Free.

---
+++ Team15Game/Assets/Scripts/EnemyDetectPlayer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyDetectPlayer : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private float distanceBetween;
+
+    private float distance;
+
+    void Update()
+    {
+        distance = Vector2.Distance(transform.position, player.transform.position); //oblicza dystans miedzy tym obiektem, a obiektem gracza
+        Vector2 direction = player.transform.position - transform.position; //I guess, ze odejmowanie dwoch wektorow da nam faktycznie kierunek gdzie sie poruszaja
+        direction.Normalize(); //dajemy wartosc wektora na 1, gosciu z poradnika mowi, ze to dlatego zeby uniknac jakichs bugow z tym zwiazanych czy cos
+
+        if (distance < distanceBetween) //scigaj gracza tylko jesli jest w odleglosci 4
+        {
+            this.gameObject.GetComponent<AIPath>().enabled = true;
+        }
+        else
+        {
+            this.gameObject.GetComponent<AIPath>().enabled = false;
+        }
+    }
+
+}
