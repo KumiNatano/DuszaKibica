60cad9da3b476eb51ab4c2737672eb738ec0448f
bloodyrise <kuba_szymcz@wp.pl>
2022-11-19 21:01:08 +0100

    w teorii ukończony system, wmiare przetestowany

--- Assets/Scripts/HealthSystemParent.cs
+++ Assets/Scripts/HealthSystemParent.cs
@@ -2,31 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealtSystemParent : MonoBehaviour
+public class HealthSystemParent : MonoBehaviour
 {
     private int healthAmount;
-    private int maxHealtAmount;
+    private int maxHealtAmount=100;
     private bool isAlive;
-    public HealthBarPlayer hpBar;
+    public BarParent hpBar;
     // Start is called before the first frame update
     void Start()
     {
         healthAmount = maxHealtAmount;
         isAlive = true;
-        hpBar.SetMaxHealth(maxHealtAmount);
-        
+        hpBar.SetMax(maxHealtAmount);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void TakeDamage(int dmg)
     {
-        healthAmount -= dmg;
-        if (healthAmount <= 0)
+        if(healthAmount-dmg >= 0)
+        {
+            healthAmount -= dmg;
+        }
+        else
+        {
+            healthAmount = 0;
+        }
+        hpBar.Change(healthAmount);
+        if (healthAmount == 0)
         {
             isAlive = false;
         }
@@ -38,6 +46,10 @@ public class HealtSystemParent : MonoBehaviour
         {
             healthAmount = maxHealtAmount;
         }
-
+        hpBar.Change(healthAmount);
+    }
+    public bool CheckIfAlive()
+    {
+        return isAlive;
     }
 }
--- Assets/Scripts/StaminaSystemParent.cs
+++ Assets/Scripts/StaminaSystemParent.cs
@@ -8,11 +8,13 @@ public class StaminaSystemParent : MonoBehaviour
     private int maxStaminaAmount = 200;
     private int refillRate = 2;
     private float timer;
+    public BarParent staminaBar;
 
     // Start is called before the first frame update
     void Start()
     {
         staminaAmount = maxStaminaAmount;
+        staminaBar.SetMax(maxStaminaAmount);
         timer = 0;
         
     }
@@ -23,16 +25,22 @@ public class StaminaSystemParent : MonoBehaviour
         if (staminaAmount<maxStaminaAmount)
             timer += Time.deltaTime;
 
-        if (timer > 5)
+        if (timer > 0.5)
         {
             RefillTick();
             timer = 0;
         }
 
     }
-    void TakeAction(int cost)
+    public bool TakeAction(int cost)
     {
-        staminaAmount -= cost;
+        if (staminaAmount - cost >= 0)
+        {
+            staminaAmount -= cost;
+            staminaBar.Change(staminaAmount);
+            return true;
+        }
+        return false;
     }
     void RefillTick()
     {
@@ -41,5 +49,6 @@ public class StaminaSystemParent : MonoBehaviour
         {
             staminaAmount = maxStaminaAmount;
         }
+        staminaBar.Change(staminaAmount);
     }
 }
