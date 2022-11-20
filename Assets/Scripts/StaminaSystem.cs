39c7a762fd10bfec9362280319611dcad5d453d8
bloodyrise <kuba_szymcz@wp.pl>
2022-11-20 01:14:42 +0100

    drobne poprawki w systemach życia i staminy + dodanie leczenie potkami

--- Assets/Scripts/StaminaSystem.cs
+++ Assets/Scripts/StaminaSystem.cs
@@ -2,19 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StaminaSystemParent : MonoBehaviour
+public class StaminaSystem : MonoBehaviour
 {
     private int staminaAmount;
-    private int maxStaminaAmount = 200;
-    private int refillRate = 2;
+    private int maxStaminaAmount;
+    private int regenRate;
     private float timer;
     public BarParent staminaBar;
 
     // Start is called before the first frame update
+    //wzasadzie to konstruktor w tym wypadku
     void Start()
     {
-        staminaAmount = maxStaminaAmount;
-        staminaBar.SetMax(maxStaminaAmount);
+        setMax(200);
+        setRegen(1);
         timer = 0;
         
     }
@@ -25,7 +26,7 @@ public class StaminaSystemParent : MonoBehaviour
         if (staminaAmount<maxStaminaAmount)
             timer += Time.deltaTime;
 
-        if (timer > 0.5)
+        if (timer > 0.05)
         {
             RefillTick();
             timer = 0;
@@ -42,13 +43,31 @@ public class StaminaSystemParent : MonoBehaviour
         }
         return false;
     }
-    void RefillTick()
+    private void RefillTick()
     {
-        staminaAmount += refillRate;
+        staminaAmount += regenRate;
         if (staminaAmount > maxStaminaAmount)
         {
             staminaAmount = maxStaminaAmount;
         }
         staminaBar.Change(staminaAmount);
+    }
+    public void setMax(int max)
+    {
+        maxStaminaAmount = max;
+        staminaAmount = maxStaminaAmount;
+        staminaBar.SetMax(maxStaminaAmount);
+    }
+    public void setRegen(int regen)
+    {
+        regenRate = regen;
+    }
+    public int getStamina()
+    {
+        return staminaAmount;
+    }
+    public int getMaxStamina()
+    {
+        return maxStaminaAmount;
     }
 }
