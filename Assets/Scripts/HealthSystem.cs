28e94f60fe20552618dc5328869178cb2ccce148
bloodyrise <kuba_szymcz@wp.pl>
2022-11-20 01:14:42 +0100

    drobne poprawki w systemach życia i staminy + dodanie leczenie potkami

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthSystemParent : MonoBehaviour
+public class HealthSystem : MonoBehaviour
 {
     private int healthAmount;
     private int maxHealtAmount=100;
