b89d7b468a285ef1eac9c47ca274ce7c701f7481
wiktorszewczyk <wiktorszewczyk21@gmail.com>
2023-01-19 19:52:13 +0100

    Bottle beta prefab, bottle spawner

--- Assets/Scripts/Player/AttackArea.cs
+++ Assets/Scripts/Player/AttackArea.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class AttackArea : MonoBehaviour
 {
     [SerializeField] private int damage = 3;
+    [SerializeField] private int baseDamage = 3;
     [SerializeField] private GameObject Player;
+    public Weapons weapons;
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
@@ -14,7 +16,14 @@ public class AttackArea : MonoBehaviour
             HealthSystem health = collider.GetComponent<HealthSystem>(); //bierzemy system zycia
             health.TakeDamage(damage); //i dajemy damage
             Player.GetComponent<PunchAudio>().PlayAttackSound(); //odgrywamy dzwiek uderzenia
-
+            weapons.checkForWeapons(); //sprawdzenie i podjęcie akcji związanych z updatem broni, obrażeń itp.
         }
+    }
+
+    public void setDamage(int newValue) {
+        damage = newValue;
+    }
+    public int getBaseDamage() {
+        return baseDamage;
     }
 }
