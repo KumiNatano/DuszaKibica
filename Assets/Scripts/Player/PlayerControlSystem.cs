3f1f803ec259a5fa3bf2c3dbab916c1b3f15130b
Diofantos232 <jakub.g.232@gmail.com>
2023-03-11 16:58:24 +0100

    Naprawienie poruszania się postaci w 3D
    + naprawa kamery.

--- Assets/Scripts/Player/PlayerControlSystem.cs
+++ Assets/Scripts/Player/PlayerControlSystem.cs
@@ -9,14 +9,8 @@ public class PlayerControlSystem : MonoBehaviour
     public float moveSpeed = 5f;
     public float collisionOffset = 0.05f;
 
-    //public ContactFilter2D movementFilter;
-
-    // Vector2 = [x, y]
-    // x - left, right
-    // y - up, down
     Vector3 movementInput;
     Rigidbody collisionShape;
-    //List<RaycastHit> castCollisions = new List<RaycastHit>();
 
     // Start is called before the first frame update
     void Start()
@@ -24,51 +18,24 @@ public class PlayerControlSystem : MonoBehaviour
         collisionShape = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    // Fixed update is called a certain number of times per second
-    // Recommended when working on physics
     private void FixedUpdate()
     {
         // If movement input is different than 0, try to move
         if (movementInput != Vector3.zero)
         {
-            bool successfullMove = TryToMove(movementInput);
-
-            if (!successfullMove)
-            {
-                successfullMove = TryToMove(new Vector3(movementInput.x, 0, 0));
-
-                if (!successfullMove)
-                {
-                    successfullMove = TryToMove(new Vector3(0, movementInput.y, 0));
-                }
-            }
+            TryToMove(movementInput);
+            
         }
     }
 
-    private bool TryToMove(Vector3 direction)
+    private void TryToMove(Vector3 direction)
     {
-        // Check for potential collisions
-        /*
-        int count = collisionShape.Cast(                            // if count = 0 the move is valid (no collisions will occur) so we can move
-            direction,                                              // X and Y values between - 1 and 1 that represent the direction from the body to look for collisions
-            movementFilter,                                         // The settings that determine where a collision can occur on; such as layers to collide with
-            castCollisions,                                         // List of collisions to store the found collisions into, after the Cast is finished
-            moveSpeed * Time.fixedDeltaTime + collisionOffset);     // The amount to cast equal to the movement plus an offset
-        */
-        if (Physics.Raycast(transform.position, direction))
+        Vector3 high = new Vector3(0f, 1f, 0f);
+        Debug.DrawRay(transform.position + high, movementInput * 2, Color.red);
+
+        if (!Physics.Raycast(transform.position + high, movementInput, 2f))
         {
             collisionShape.MovePosition(transform.position + direction * moveSpeed * Time.fixedDeltaTime);
-            return true;
-        }
-        else
-        {
-            return false;
         }
     }
 
