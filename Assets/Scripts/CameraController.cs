ed2bf1aefea0d56e99d558092abd7ff5fd152ae4
wiktorszewczyk <wiktorszewczyk21@gmail.com>
2023-03-12 21:36:50 +0100

    Merge

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -14,40 +14,21 @@ public class CameraController : MonoBehaviour
     public float lowerEgde = -6;
 
     public Camera MainCamera;
-    public Camera TPPCamera;
 
-
-    public bool isTPEnabled = false;
     Quaternion rotationBeforeSwitch;
 
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 vector = new Vector3(0f, high , - 1f); // w tym miejscu zmieniamy Y by ustawic k¹t kamery
+        Vector3 vector = new Vector3(0f, high , - 1f); // w tym miejscu zmieniamy Y by ustawic kï¿½t kamery
         transform.position = player.transform.position + vector;
         transform.LookAt(player);
-        TPPCamera.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Keypad9))
-        {
-            isTPEnabled = !isTPEnabled;
-            MainCamera.enabled = !MainCamera.enabled;
-            TPPCamera.enabled = !TPPCamera.enabled;
-            player.gameObject.GetComponent<PlayerControlSystem>().enabled = !player.gameObject.GetComponent<PlayerControlSystem>().enabled;
-            player.gameObject.GetComponent<RotateToPointer>().enabled = !player.gameObject.GetComponent<RotateToPointer>().enabled;
-            player.gameObject.GetComponent<TPPPlayerMovement>().enabled = !player.gameObject.GetComponent<TPPPlayerMovement>().enabled;
-        }
 
-        if(isTPEnabled == true)
-        {
-            //TPCamera();
-        }
-
-        else
         {
             followPlayer();
         }
@@ -81,15 +62,5 @@ public class CameraController : MonoBehaviour
             transform.position = newPosition;
         }
     }
-
-    void TPCamera()
-    {
-        Transform cameraTransform = this.transform;
-        Transform playerTransform = player.transform;
-
-        this.transform.rotation = Quaternion.Euler(playerTransform.rotation.x - 50, playerTransform.rotation.y, playerTransform.rotation.z);
-        this.transform.position = new Vector3(playerTransform.position.x, player.transform.position.y - 5, playerTransform.position.z - 3);
-
-}
     
 }
