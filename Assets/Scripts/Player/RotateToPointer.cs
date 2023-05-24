4c88b39c9b9499ca419ac1fd93f20d05145879cc
Wiktor Czernik <git@czernik.dev>
2023-05-24 17:21:48 +0200

    Naprawiłem wejście myszy zależne od FPS

--- Assets/Scripts/Player/RotateToPointer.cs
+++ Assets/Scripts/Player/RotateToPointer.cs
@@ -7,14 +7,16 @@ public class RotateToPointer : MonoBehaviour
     [SerializeField] Camera fppCamera;
     [SerializeField] GameObject playerModel;
 
-    // Czu³oœæ ruchu myszy
-    public float mouseSensitivityX = 100f;
-    public float mouseSensitivityY = 100f;
 
-    // Transformacja cia³a gracza
+    
+    // CzuÅ‚oÅ›Ä‡ ruchu myszy
+    public float mouseSensitivityX = 5f;
+    public float mouseSensitivityY = 5f;
+
+    // Transformacja ciaÅ‚a gracza
     public Transform playerBody;
 
-    // Aktualna wartoœæ rotacji kamery wokó³ osi X
+    // Aktualna wartoÅ›Ä‡ rotacji kamery wokÃ³ osi X
     private float xRotation = 0f;
 
     private float yRotation = 0f;
@@ -33,28 +35,14 @@ public class RotateToPointer : MonoBehaviour
             Cursor.lockState = CursorLockMode.Locked;
         }
 
-        // Pobierz wartoœæ ruchu myszy po osi X i przemnó¿ przez czu³oœæ ruchu myszy
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivityX * Time.deltaTime;
-
-        // Pobierz wartoœæ ruchu myszy po osi Y i przemnó¿ przez czu³oœæ ruchu myszy
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivityY * Time.deltaTime;
-
-        // Dodaj wartoœæ ruchu myszy po osi X do aktualnej rotacji kamery wokó³ osi Y
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivityX;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivityY;
+        
         yRotation -= mouseX;
+        xRotation = Mathf.Clamp(xRotation - mouseY, -90f, 90f);
 
-        // Odejmij wartoœæ ruchu myszy po osi Y od aktualnej wartoœci rotacji kamery wokó³ osi X
-        xRotation -= mouseY;
-
-        // Ogranicz wartoœæ rotacji kamery wokó³ osi X do zakresu od -90 do 90 stopni
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-        // Utwórz nowy Quaternion na podstawie aktualnej rotacji kamery
         Quaternion cameraRotation = Quaternion.Euler(xRotation, 0f, 0f);
-
-        // Obróæ cia³o gracza wokó³ osi Y zgodnie z ruchem myszy
         playerBody.Rotate(Vector3.up * mouseX);
-
-        // Ustaw now¹ rotacjê kamery
         fppCamera.transform.localRotation = cameraRotation;
     }
 }
