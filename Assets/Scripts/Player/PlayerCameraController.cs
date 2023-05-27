037beb378f353be6a753b8dcc09a153aad255904
Wiktor Czernik <git@czernik.dev>
2023-05-27 11:11:03 +0200

    PlayerCamera i moduł PlayerCameraController, poruszanie się i bicie tymczasowo zepsute

---
+++ Assets/Scripts/Player/PlayerCameraController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerCameraController : PlayerModule
+{
+    // Referencja do kamery gracza
+    public PlayerCamera playerCamera;
+    // Czułość ruchu myszy
+    public Vector2 sensitivity = new Vector2(5f, 5f);
+    // Krzywa reprezentująca zmianę w przyśpieszniu myszy w zalezności od długości delty ruchu myszy
+    public AnimationCurve accelerationCurve = new AnimationCurve(new Keyframe(0, 0), new Keyframe(1, 1));
+
+    public bool accelerate = true;
+
+    public Transform eyesPoint;
+
+
+    public override void OnLateUpdate(float deltaTime)
+    {
+        playerCamera.SetPosition(eyesPoint.position);
+
+        Vector2 input = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        if (accelerate){
+            input *= accelerationCurve.Evaluate(input.magnitude);
+        }
+        input *= sensitivity;
+        
+        Vector3 viewAngles = playerCamera.viewAngles;
+        viewAngles.y += input.x;
+        viewAngles.x = Mathf.Clamp(viewAngles.x - input.y, -90f, 90f);
+
+        playerCamera.SetViewAngles(viewAngles);
+    }
+}
