ce4c14f78dcd2d6b26fd543b056f03c8e95320bf
Wiktor Czernik <git@czernik.dev>
2023-05-27 14:18:58 +0200

    Bieganie, wyłączono komponent PlayerControlSystem

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : PlayerModule
 {
     public float walkSpeed = 5f;
+    public float runSpeed = 7.5f;
 
     public PlayerCamera playerCamera => parent.playerCamera;
     public CharacterController controller => parent.characterController;
@@ -18,12 +19,12 @@ public class PlayerController : PlayerModule
     private void Move(){
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         float vy = playerCamera.viewAngles.y;
-        float speed = walkSpeed;
+        float speed = (Input.GetButton("Sprint") && input.y > float.Epsilon) ? runSpeed : walkSpeed;
         Vector3 dir = AngleToDir(vy) * input.y + AngleToDir(vy + 90f) * input.x;
 
         dir.Normalize();
 
-        controller.SimpleMove(dir * walkSpeed);
+        controller.SimpleMove(dir * speed);
     }
     private Vector3 AngleToDir(float angle){
         return Quaternion.Euler(Vector3.up * angle) * Vector3.forward;
