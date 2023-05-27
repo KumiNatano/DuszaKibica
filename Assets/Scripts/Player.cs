70baa923fa23c45725ce60d74a26720b19ee9723
Wiktor Czernik <git@czernik.dev>
2023-05-27 10:27:46 +0200

    Zdefiniowałem ogólny moduł gracza (PlayerModule) oraz klasę gracza (Player)

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using System.Linq;
+
+public class Player : MonoBehaviour
+{
+    public T GetModule<T>() where T : PlayerModule
+    {
+        return (T)modules.First(x => x is T);
+    }
+    public bool TryGetModule<T>(out T module) where T : PlayerModule
+    {
+        T m = (T)modules.FirstOrDefault(x => x is T);
+        if (m == null || m == default(T)){
+            module = null;
+            return false;
+        }
+        module = m;
+        return true;
+    }
+
+    #region  Unity Callbacks
+    private void Awake()
+    {
+        InitModules();
+    }
+    private void Update()
+    {
+        ModulesUpdate();
+    }
+    private void FixedUpdate()
+    {
+        ModulesFixedUpdate();
+    }
+    private void LateUpdate()
+    {
+        ModulesLateUpdate();
+    }
+    #endregion
+
+    #region Modules
+    private void InitModules()
+    {
+        // find a better way to add camera to modules list
+        modules = GetComponentsInChildren<PlayerModule>();
+        foreach (var mod in modules)
+        {
+            if (!mod.Init(this)){
+                Debug.LogError($"Could not initialize {mod.GetType().Name} module!");
+            }
+        }
+    }
+    private void ModulesUpdate()
+    {
+        foreach (PlayerModule mod in modules)
+        {
+            mod.OnUpdate(Time.deltaTime);
+        }
+    }
+    private void ModulesFixedUpdate()
+    {
+        foreach (PlayerModule mod in modules)
+        {
+            mod.OnFixedUpdate(Time.fixedDeltaTime);
+        }
+    }
+    private void ModulesLateUpdate()
+    {
+        foreach (PlayerModule mod in modules)
+        {
+            mod.OnLateUpdate(Time.deltaTime);
+        }
+    }
+    #endregion
+
+    private PlayerModule[] modules; 
+}
