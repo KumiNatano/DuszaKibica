12fcc292b59953d56f447d8c13a77d6858d49488
Jacek112 <jacekteodorczyk01@gmail.com>
2023-05-29 20:31:52 +0200

    Dodanie menu i zmainy w rzucaniu noża.

--- Assets/Scripts/Animations/PlayerAnimations.cs
+++ Assets/Scripts/Animations/PlayerAnimations.cs
@@ -19,7 +19,7 @@ public class PlayerAnimations : MonoBehaviour
     public void setIsAttackingLeftTrue()
     {
         animator.SetBool("isAttackingLeft", true);
-        print("bije lewa");
+        //print("bije lewa");
     }
 
     public void setIsAttackingLeftFalse()
