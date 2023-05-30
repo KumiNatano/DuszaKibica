f9ee22ccc5ef096c37628e81fafcf2be309558de
Viktor Chernikov <viktor.chernikov@onet.pl>
2023-05-30 20:05:05 +0200

    Headbobbing dla biegania i kucania

--- Assets/Scripts/Player/PlayerHeadbob.cs
+++ Assets/Scripts/Player/PlayerHeadbob.cs
@@ -4,20 +4,37 @@ using UnityEngine;
 
 public class PlayerHeadbob : PlayerModule
 {
+    public float runAmplitude = 0.075f;
+    public float runFrequency = 12f;
     public float walkAmplitude = 0.05f;
     public float walkFrequency = 8f;
+    public float duckAmplitude = 0.03f;
+    public float duckFrequency = 4f;
 
     PlayerCamera playerCamera => parent.playerCamera;
+    PlayerController controller => parent.controller;
 
 
     public override void OnLateUpdate(float deltaTime)
     {
         Vector3 velocity = parent.characterController.velocity;
         velocity.y = 0;
-        if (velocity.magnitude > float.Epsilon && !parent.controller.isDucking)
+        if (velocity.magnitude > float.Epsilon)
         {
+            float frequency = walkFrequency;
+            float amplitude = walkAmplitude;
+            if (controller.isRunning)
+            {
+                frequency = runFrequency;
+                amplitude = runAmplitude;
+            }
+            else if (controller.isDucking)
+            {
+                frequency = duckFrequency;
+                amplitude = duckAmplitude;
+            }
             Vector3 newPos = playerCamera.GetPosition();
-            newPos.y += Mathf.Sin(Time.time * walkFrequency) * walkAmplitude;
+            newPos.y += Mathf.Sin(Time.time * frequency) * amplitude;
             playerCamera.SetPosition(newPos);
         }
     }
