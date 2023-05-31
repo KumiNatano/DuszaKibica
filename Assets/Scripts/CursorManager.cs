2ede808213e36aaea33314500fb4985b3da80d11
Wiktor Czernik <git@czernik.dev>
2023-05-31 16:46:49 +0200

    CursorManager blokuje kursor najs

---
+++ Assets/Scripts/CursorManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    public PauseManager pause;
+    public PlayerDeathManager deathm;
+    void Update()
+    {
+        if (pause.isPaused || deathm.isDead){
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+}
