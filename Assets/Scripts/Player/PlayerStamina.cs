75031190d4d2a52b51064d35eaf11b334c56e1ef
Viktor Chernikov <viktor.chernikov@onet.pl>
2023-06-01 17:58:20 +0200

    Zdefiniowano nowy moduł PlayerStamina który zastąpi StaminaSystem

---
+++ Assets/Scripts/Player/PlayerStamina.cs
@@ -0,0 +1,71 @@
+using System;
+using UnityEngine;
+
+public class PlayerStamina : PlayerModule
+{
+    public float points = 100.0f;
+    public float maxPoints = 100.0f;
+
+    [Header("Regeneration")]
+    public float regenPoints = 10;
+    public float regenAfter = 3f;
+
+    public const int regenRate = 16;
+    public float regenRateRaw => 1.0f / regenRate;
+    public float lastPointUse { get; private set; } = 0f;
+
+    public event Action<float> onUse;
+    public event Action<float> onRegenerate;
+    public event Action<float, float> onPointsChange;
+
+
+    public override void OnInit()
+    {
+        InvokeRepeating(nameof(RegenTick), 0f, regenRateRaw);
+    }
+
+    public bool UsePoints(float amount) 
+    {
+        lastPointUse = Time.time;
+        if (points < amount)
+        {
+            return false;
+        }
+        float old = points;
+        points -= amount;
+        points = Mathf.Clamp(points, 0, maxPoints);
+        if (old != points)
+        {
+            onPointsChange?.Invoke(old, points);
+            onUse?.Invoke(old - points);
+        }
+        return true;
+    }
+    public void Regenerate(float amount)
+    {
+        float old = points;
+        points += amount;
+        if (points > maxPoints)
+        {
+            points = maxPoints;
+        }
+        if (old != points)
+        {
+            onPointsChange?.Invoke(old, points);
+            onRegenerate?.Invoke(points - old);
+        }
+    }
+
+    void RegenTick()
+    {
+        float slu = Time.time - lastPointUse;
+        if (slu < regenAfter)
+        {
+            return;
+        }
+        if (points < maxPoints)
+        {
+            Regenerate(regenPoints * regenRateRaw);
+        }
+    }
+}
