a5f772c954a7168e4b624be99be6adc9e23392f9
Viktor Chernikov <viktor.chernikov@onet.pl>
2023-06-11 20:19:35 +0200

    Wielka czystka w PlayerAttack. FuryAbility nie działa poprawnie, usunięto logikę celownika z playerattack

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -13,103 +13,27 @@ public class PlayerAttack : PlayerModule
     /// </summary>
     public PlayerPunchMachine rightArm => _rightArm;
 
-
-    private GameObject attackArea = default;
-
-    private bool isAttacking = false; //czy aktualnie jest w fazie ataku
-
-    private float timeToAttack = 0.25f; //jak dlugo ma byc otwarte okienko czasowe w ktorym mozna atakowac (jak dlugo AttackArea bedzie dostepne)
-    private float timer = 0f; //pomocniczy timer
-
-    [SerializeField] public float cooldown = 2f;
-    public bool canAttack = true;
-
-    [SerializeField] private GameObject swish;
-    private GameObject crosshair;
+    public bool canAttack => leftArm.isIdling || rightArm.isIdling;
 
 
     public override void OnInit()
     {
-        attackArea = transform.GetChild(0).gameObject; //przypisujemy pierwsze dziecko tego obiektu jako attackArea
-        //animator = transform.GetChild(2).gameObject.GetComponent<Animator>();
-        crosshair = GameObject.FindWithTag("Crosshair");
+        leftArm.Init(parent);
+        rightArm.Init(parent);
     }
     public override void OnUpdate(float deltaTime)
     {
-        if (canAttack && this.gameObject.GetComponent<FuryAbility>().isInAnimation == false)
+        // TODO: uzyc Input.GetButtonDown()
+        if (Input.GetMouseButtonDown(0) && !rightArm.isAttacking)
         {
-            crosshair.GetComponent<uGUI_Crosshair>().synchronizationLeft = true; //musi to byc ustawiane stad, poniewaz jesli w uGUI_Crosshair bedziemy chcieli pobrac wartosc
-                                                                      //stad to sie rozwali ze wzgledu na to, ze calosc dzieje sie co klatke i tamten skrypt nie nadaza
-                                                                      
-            crosshair.GetComponent<uGUI_Crosshair>().synchronizationRight = true; //to samo dla prawej, poki co obie tutaj bo nie ma rozroznienia na prawa i lewa reke
+            leftArm.Attack();
         }
-        
-        Vector3 c = attackArea.GetComponent<SphereCollider>().center;
-        swish.transform.position = attackArea.transform.position + c;
-        c.y = 0;
-        Vector3 r = swish.transform.eulerAngles;
-        r.y = Quaternion.LookRotation(c).eulerAngles.y;
-        swish.transform.eulerAngles = r;
-        if (Input.GetButton("Fire1") && canAttack == true && GameObject.FindWithTag("PauseManager").GetComponent<PauseManager>().isPaused == false) //jesli nacisniemy i mamy mozliwosc ataku i nie ma pauzy
+        else if (Input.GetMouseButtonDown(1) && !leftArm.isAttacking) 
         {
-            Attacking(); //to zaczynamy atak
-            StartCoroutine(AttackCoroutine()); //i korutyne
-        }
-
-        if (isAttacking) //jesli jestesmy w trakcie ataku
-        {
-            timer += deltaTime;
-
-            if(timer >= timeToAttack) //jesli sie skonczy okienko czasowe na atak to wylacza nam obiekt dziecka
-            {
-                timer = 0;
-                isAttacking = false;
-                attackArea.SetActive(isAttacking);
-                swish.SetActive(isAttacking);
-            }
+            rightArm.Attack();
         }
     }
 
-    IEnumerator AttackCoroutine()
-    {
-        canAttack = false; //zmienia, ze nie mozemy zaatakowac
-        yield return new WaitForSeconds(cooldown); //czekaj "cooldown" sekund zanim wykona instrukcje ponizej
-        canAttack = true; //a po czasie juz mozna
-    }
-
-
-    private void Attacking()
-    {
-        if(this.gameObject.GetComponent<PlayerDeathManager>().isDead == false && this.gameObject.GetComponent<FuryAbility>().isInAnimation == false)
-        {
-            isAttacking = true;
-            attackArea.SetActive(isAttacking);
-            this.gameObject.GetComponent<PunchAudio>().PlaySwishSound(); //odtwarzanie dzwieku machniecia
-            swish.SetActive(isAttacking);
-
-            int animacjaAtaku = Random.Range(1, 3);
-
-            if (animacjaAtaku == 1)
-            {
-                this.gameObject.GetComponent<PlayerAnimations>().setIsAttackingLeftTrue();
-            }
-
-            else if (animacjaAtaku == 2)
-            {
-                this.gameObject.GetComponent<PlayerAnimations>().setIsAttackingRightTrue();
-            }
-
-            StartCoroutine(turnOffAnimation());
-        }
-        
-    }
-
-    IEnumerator turnOffAnimation()
-    {
-        yield return new WaitForSeconds(0.311f); //idealnie dlugosc animacji uderzania, w przyszlosci lepiej wrzucic to do zmiennej jesli beda np animacje o roznej dlugosci
-        this.gameObject.GetComponent<PlayerAnimations>().setIsAttackingLeftFalse();
-        this.gameObject.GetComponent<PlayerAnimations>().setIsAttackingRightFalse();
-    }
 
     [Header("Arms")]
     [SerializeField] PlayerPunchMachine _leftArm;
