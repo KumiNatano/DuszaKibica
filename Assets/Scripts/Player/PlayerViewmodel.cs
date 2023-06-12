48fa5b80eb0d8244b97dc93c5132ca19075b0ff0
Wiktor Czernik <git@czernik.dev>
2023-06-12 18:50:43 +0200

    Viewmodel ma teraz swój moduł

---
+++ Assets/Scripts/Player/PlayerViewmodel.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerViewmodel : PlayerModule
+{
+    PlayerAttack attack;
+
+
+    public override void OnInit()
+    {
+        animator = GetComponent<Animator>();
+        attack = parent.GetModule<PlayerAttack>();
+
+        attack.leftArm.onAttackBegin += PunchLeft;
+        attack.rightArm.onAttackBegin += PunchRight;
+    }
+
+    public void PunchRight() => Punch(true, attack.rightArm.attackDuration);
+    public void PunchLeft() => Punch(false, attack.leftArm.attackDuration);
+    public void Punch(bool right, float duration)
+    {
+        string hand = "Punch " + (right ? "Right" : "Left");
+        duration = 1f / duration;
+        animator.SetFloat(hand + " Speed", duration);
+        animator.SetTrigger(hand);
+    }
+
+
+    Animator animator;
+}
