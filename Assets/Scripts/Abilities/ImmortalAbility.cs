4b0d00146617beaca29f4efe384d318e3d98e093
Wiktor Czernik <git@czernik.dev>
2023-06-17 00:17:21 +0200

    Oznaczyłem stare implementacje umiejętności jako przestarzałe

--- Assets/Scripts/Abilities/ImmortalAbility.cs
+++ Assets/Scripts/Abilities/ImmortalAbility.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[Obsolete]
 public class ImmortalAbility : MonoBehaviour
 {
     [SerializeField] UpgradeController upgradeController;
