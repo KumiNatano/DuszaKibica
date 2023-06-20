8266cb33468d7bdc6607cb837ce2bb57cbeeefb8
Wiktor Czernik <git@czernik.dev>
2023-06-20 21:41:59 +0200

    Poprawiłem coś w klasie ragdoll i dodałem metodę push

--- Assets/Scripts/Ragdoll.cs
+++ Assets/Scripts/Ragdoll.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Linq;
+using System.Collections.Generic;
 
 public class Ragdoll : MonoBehaviour
 {
@@ -8,18 +9,27 @@ public class Ragdoll : MonoBehaviour
     public void Activate()
     {
         animator.enabled = false;
-        GetComponentsInChildren<Rigidbody>().ToList().ForEach(x => x.isKinematic = false);
-        GetComponentsInChildren<Collider>().ToList().ForEach(x => x.enabled = true);
+        rigidbodies.ForEach(x => x.isKinematic = false);
+        colliders.ForEach(x => x.enabled = true);
     }
     public void Deactivate()
     {
         animator.enabled = true;
-        GetComponentsInChildren<Rigidbody>().ToList().ForEach(x => x.isKinematic = true);
-        GetComponentsInChildren<Collider>().ToList().ForEach(x => x.enabled = false);
+        rigidbodies.ForEach(x => x.isKinematic = true);
+        colliders.ForEach(x => x.enabled = false);
+    }
+    public void Push(Vector3 force)
+    {
+        rigidbodies.First().AddForce(force);
     }
 
     void Awake()
     {
         animator = GetComponent<Animator>();
+        rigidbodies = GetComponentsInChildren<Rigidbody>().ToList();
+        colliders = GetComponentsInChildren<Collider>().ToList();
     }
+
+    List<Rigidbody> rigidbodies;
+    List<Collider> colliders;
 }
