8eddf700b6a10911d960970f4d7d97030e3125eb
Wiktor Czernik <git@czernik.dev>
2023-06-21 22:40:48 +0200

    Jest pokazywany czas ile zostało do odnowienia umiejętności

--- Assets/Scripts/uGUI/HUD/uGUI_AbilityElement.cs
+++ Assets/Scripts/uGUI/HUD/uGUI_AbilityElement.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using System.Text;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -34,9 +36,43 @@ public class uGUI_AbilityElement : MonoBehaviour
             icon.sprite = restSprite;
         }
     }
+    void UpdateTimer()
+    {
+        float num1 = abilityRef.restDuration;
+        float num2 = abilityRef.restFraction;
+        bool flag = true;
+
+        sb.Append(num1 - num1 * num2);
+        for (int i = 0; i < sb.Length; i++)
+        {
+            if (sb[i] == '.')
+            {
+                i += 2;
+                sb.Remove(i, sb.Length - i);
+                flag = false;
+                break;
+            }
+        }
+        if (flag)
+        {
+            sb.Append(".0");
+        }
+        sb.Append("s");
+        timer.text = sb.ToString();
+        sb.Clear();
+    }
+    void HideTimer()
+    {
+        timer.text = "";
+    }
 
     private void Start()
     {
         abilityRef.onStateUpdate += UpdateIcon;
+        abilityRef.onRestBegin += UpdateTimer;
+        abilityRef.onRestStay += UpdateTimer;
+        abilityRef.onRestEnd += HideTimer;
     }
+
+    StringBuilder sb = new StringBuilder();
 }
