6bf2b6dce3d9e6bc9d0eff3a688fc790034749d5
wiktorszewczyk <wiktorszewczyk21@gmail.com>
2023-06-21 16:01:02 +0200

    feat: Wskaznik na wroga (placeholder)

---
+++ Assets/Scripts/UI/TargetController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetController : MonoBehaviour
+{
+    public Transform target;
+    [SerializeField] private float hideDistance = 5f; // Odległość przy której noże w stronę przeciwnika znikają.
+
+    private void SetIndicatorActive(bool isActive) {
+        foreach (Transform child in transform) {
+            child.gameObject.SetActive(isActive);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var direction = target.position - transform.position;
+        var angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + 90;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.up); // Vector3.up - wzdłuż osi y
+
+        if (direction.magnitude <= hideDistance) {
+            SetIndicatorActive(false);
+        } else {
+            SetIndicatorActive(true);
+        }
+    }
+}
